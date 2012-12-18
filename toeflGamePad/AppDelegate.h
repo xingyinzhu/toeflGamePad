86f984bb80e4259bc92583c8f912cb4497cb8b3d
XingyinZhu <xingyinzhu@gmail.com>
2012-12-18 20:00:38 +0800

    Initial Commit

---
+++ toeflGamePad/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  toeflGamePad
+//
+//  Created by Xingyin Zhu on 12-12-18.
+//  Copyright (c) 2012年 Xingyin Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
